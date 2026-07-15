b7b94205c9b190f49c3640179f839bc0afa55eb0
heniums <hennv404@gmail.com>
2026-07-15 13:53:33 +0700

    chore(test): add global ResizeObserver mock, update task statuses

--- client/test/setup.ts
+++ client/test/setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom/vitest';
+
+// jsdom does not implement ResizeObserver (used by Radix ScrollArea)
+global.ResizeObserver = class ResizeObserver {
+  observe(): void {}
+  unobserve(): void {}
+  disconnect(): void {}
+};
